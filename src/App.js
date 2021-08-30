388d8725540e05e03043e3c1fa17f6c40065ee16
tuukka <tuukka.pohjonen@outlook.com>
2021-08-30 19:58:33 +0300

    Exercises 1.1-1.2

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.content.name} {props.content.exercises}</p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part content={props.parts[0]} />
+      <Part content={props.parts[1]} />
+      <Part content={props.parts[2]} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.total}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
+    </div>
+  )
+}
+
+export default App
